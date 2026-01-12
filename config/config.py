2796ec9ac3567193f70b2a5cd4a17a5c1670eb3a
Vineet Raj <NH3731@nslhub.in>
2026-01-12 16:04:59 +0530

    Add INVALID_USER for negative login test

--- config/config.py
+++ config/config.py
@@ -2,3 +2,6 @@ class Config:
     BASE_URL = "https://www.saucedemo.com/"
     VALID_USER = "standard_user"
     VALID_PASSWORD = "secret_sauce"
+
+    # Negative test user
+    INVALID_USER = "locked_out_user"
